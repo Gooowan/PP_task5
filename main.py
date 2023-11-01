14552de84b7b05e918707d16b5f6d5543e3ee8f9
BUNJEE <vsolomatin@kse.org.ua>
2023-11-01 08:56:29 +0200

    added list comprehensions

--- main.py
+++ main.py
@@ -1,35 +1,57 @@
 import string
 
 
-def encrypt(rawText, key):
-    cipher = ""
-    key = key % 26
+def encrypt_char(char, key):
+    if char.isalpha():
+        alpha_start = ord('a') if char.islower() else ord('A')
+        return chr((ord(char) - alpha_start + key) % 26 + alpha_start)
+    else:
+        return chr(ord(char)+key)
 
-    for char in rawText:
-        if char.isalpha():
-            alpha_start = ord('a') if char.islower() else ord('A')
-            shift = (ord(char) - alpha_start + key) % 26
-            cipher += chr(alpha_start + shift)
-        else:
-            cipher += chr(ord(char)+key)
-    return cipher
+
+def encrypt(rawText, key):
+    return ''.join([encrypt_char(char, key) for char in rawText])
+
+
+def decrypt_char(char, key):
+    if char.isalpha():
+        alpha_start = ord('a') if char.islower() else ord('A')
+        return chr((ord(char) - alpha_start - key + 26) % 26 + alpha_start)
+    else:
+        return chr(ord(char)-key)
 
 
 def decrypt(cipher, key):
-    rawText = ""
-    key = key % 26
+    return ''.join([decrypt_char(char, key) for char in cipher])
 
-    for char in cipher:
-        if char.isalpha():
-            alpha_start = ord('a') if char.islower() else ord('A')
-            shift = (ord(char) - alpha_start - key + 26) % 26
-            rawText += chr(alpha_start + shift)
+
+def main():
+    while True:
+        print("1. Encrypt")
+        print("2. Decrypt")
+        print("3. Exit")
+        print("Enter your command: ", end="")
+        choice = input()
+        if choice == "1":
+            print("Enter the text to encrypt: ", end="")
+            rawText = input()
+            print("Enter the key: ", end="")
+            key = int(input())
+            print("Encrypted text: ", end="")
+            print(encrypt(rawText, (key % 26)))
+        elif choice == "2":
+            print("Enter the text to decrypt: ", end="")
+            cipher = input()
+            print("Enter the key: ", end="")
+            key = int(input())
+            print("Decrypted text: ", end="")
+            print(decrypt(cipher, (key % 26)))
+        elif choice == "3":
+            break
         else:
-            rawText += chr(ord(char)-key)
-    return rawText
+            print("Invalid command")
+            continue
 
 
-a = encrypt("Hello, World!", 3)
-print(a)
-a = decrypt(a,3)
-print(a)
+if __name__ == "__main__":
+    main()
