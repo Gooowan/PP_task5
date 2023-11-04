b83cf7809621b46319fda93ac3c9ced04d24873d
BUNJEE <vsolomatin@kse.org.ua>
2023-11-04 11:56:39 +0200

    added buffer, reading from a file not working

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-import string
-
-
 def encrypt_char(char, key):
     if char.isalpha():
         alpha_start = ord('a') if char.islower() else ord('A')
@@ -25,32 +22,74 @@ def decrypt(cipher, key):
     return ''.join([decrypt_char(char, key) for char in cipher])
 
 
+def read_from_file(file_name):
+    try:
+        with open(file_name, 'r', encoding='utf-8') as file:
+            return file.read()
+    except FileNotFoundError:
+        print(f"The file {file_name} was not found.")
+        return None
+    except Exception as e:
+        print(f"An error occurred: {e}")
+        return None
+
+
+def write_to_file(file_name, text):
+    try:
+        with open(file_name, 'w', encoding='utf-8') as file:
+            file.write(text)
+        print(f"Text written to {file_name}")
+    except Exception as e:
+        print(f"An error occurred: {e}")
+
+
 def main():
+    buffer = ""
     while True:
         print("1. Encrypt")
         print("2. Decrypt")
         print("3. Exit")
-        print("Enter your command: ", end="")
-        choice = input()
+        print("4. Read from file")
+        print("5. Write to file")
+        choice = input("Enter your command: ")
+
         if choice == "1":
-            print("Enter the text to encrypt: ", end="")
-            rawText = input()
-            print("Enter the key: ", end="")
-            key = int(input())
-            print("Encrypted text: ", end="")
-            print(encrypt(rawText, (key % 26)))
+            rawText = input("Enter the text to encrypt or 'r' to use the text from the last read file: ")
+            key = int(input("Enter the key: ")) % 26
+            if rawText.upper() == 'r':
+                buffer = encrypt(buffer, key)
+            else:
+
+                buffer = encrypt(rawText, key)
+                print("Encrypted text: " + buffer)
+
         elif choice == "2":
-            print("Enter the text to decrypt: ", end="")
-            cipher = input()
-            print("Enter the key: ", end="")
-            key = int(input())
-            print("Decrypted text: ", end="")
-            print(decrypt(cipher, (key % 26)))
+            cipher = input("Enter the text to decrypt or 'r' to use the text from the last read file: ")
+            key = int(input("Enter the key: ")) % 26
+            if cipher.upper() == 'r':
+                buffer = decrypt(buffer, key)
+            else:
+                buffer = decrypt(cipher, key)
+                print("Decrypted text: " + buffer)
+
         elif choice == "3":
+            file_name = input("Enter the file name to read from: ")
+            text = read_from_file(file_name)
+            if text is not None:
+                buffer = text
+                print("Text read from file.")
+
+        elif choice == "4":
+            if not buffer:
+                print("No text available to write. Please encrypt or decrypt some text first.")
+                continue
+            file_name = input("Enter the file name to write to: ")
+            write_to_file(file_name, buffer)
+
+        elif choice == "5":
             break
         else:
             print("Invalid command")
-            continue
 
 
 if __name__ == "__main__":
